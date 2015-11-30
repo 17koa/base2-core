9930c4608622abe5b8db8c0938ead3bfd9614463
i5ting <i5ting@126.com>
2015-11-30 23:29:53 +0800

    init

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-var express = require('express');
-var bodyparser = require('body-parser');
-var path = require('path');
+var express     = require('express');
+var bodyparser  = require('body-parser');
+var path        = require('path');
+var mount       = require('mount-routes');
+var app         = express();
 
-var app = express();
-var lib = require('./lib');
 
 app.set('port', 8001);
 app.set('root', path.join(__dirname, '..'));
 app.use('/public', express.static(path.join(__dirname, '../public')));
 app.use(bodyparser.urlencoded({ extended: false }));
- 
-for(var k in lib){
-  var v = lib[k];
-  
-  if(v){
-    var middleware = v(app);
-    
-    if(middleware){
-      app.use(middleware);
-    }else{
-      console.log('empty middleware');
-    }
-  }
-}
+
+// simple
+// mount(app);
+
+// with path
+// mount(app, 'routes2');
+require('./lib')(app);
+// with path & api dump
+mount(app, 'routes2', true);
+
 
 module.exports = app;
+
