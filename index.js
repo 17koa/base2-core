2d66f793ba6bb167f63baa9957f59acd49dce597
i5ting <i5ting@126.com>
2015-12-01 14:58:02 +0800

    refact

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-
-// function () {
-//   var port = app.get('port');
-//   if (arguments.length >= 1) {
-//     port = arguments[0];
-//   }
-//
-//   if (!module.parent) {
-//     return app.listen(port, function () {
-//       console.log('Base2 start at port %d', app.get('port'));
-//     });
-//   }
-//
-//   process.on('uncaughtException', function (err) {
-//     console.error('Uncaught exception:', err, err.stack);
-//     // Exception
-//     switch (err.errno) {
-//       case 'EADDRINUSE': // 端口被占用
-//         process.exit(5001);
-//         break;
-//       default:
-//       //
-//     }
-//   });
-// };
-
-
 module.exports = function (config) {
   var app = require('./app')(config);
   
