8ba8d6b55509d33dfdb6b5661d25d17ea2e89a6e
i5ting <i5ting@126.com>
2015-12-02 06:33:00 +0800

    refact

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ var app         = express();
 module.exports = function (config) {
   var deepExtend = require('deep-extend');
   deepExtend(app, {
-    express: express
+    express: express,
+    set_absolute_path: function (key, path) {
+      this.set(key, app.get('root') + "/" + path); 
+    },
+    set_key_with_setting_key: function (key, setting_key) {
+      this.set(key, app.get('root') + "/" + app.get(setting_key)); 
+    }
   });
   
   var cfg = {
@@ -55,10 +61,12 @@ module.exports = function (config) {
 function _settings(cfg, app){
   app.set('port', 8001);
   app.set('public', cfg.public);
-  app.set('www', app.get('root') + "/" + app.get('public')); 
+  // app.set('www', app.get('root') + "/" + app.get('public')); 
+  app.set_key_with_setting_key('www', 'public');
+  app.set_key_with_setting_key('views', 'views');
   console.log(app.get('www'))
   
-  if(cfg.routes){
+  if (cfg.routes) {
     app.set('routes', cfg.routes);
     mount(app, app.get('root') + "/" + cfg.routes, true);
   }
