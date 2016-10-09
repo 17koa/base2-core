8964e30436b85f3e0fa51114214a8dc1093b1cfe
i5ting <i5ting@126.com>
2016-10-09 23:30:17 +0800

    refact, please ignore

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ var mount_koa_routes      = require('mount-koa-routes');
 module.exports = function (config) {
   var mount_routes = mount_express_routes;
   
-  if(config.type == 'koa'){
+  if(config.type == 'koa' || config.type == 'koa2'){
     mount_routes = mount_koa_routes;
   }
   
